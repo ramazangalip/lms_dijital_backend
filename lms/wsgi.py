33ade43b56750c09ce1fbd6580ef1e19bb5e012a
Ramazan  Said GALİP <107424417+ramazangalip@users.noreply.github.com>
2026-02-25 12:27:04 +0300

    vercel deploy geri alındı

--- lms/wsgi.py
+++ lms/wsgi.py
@@ -14,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.settings')
 
 application = get_wsgi_application()
-app = application  # Vercel'in tanıması için bu satırı ekle
